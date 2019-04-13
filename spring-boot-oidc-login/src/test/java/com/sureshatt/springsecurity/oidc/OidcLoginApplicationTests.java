48773b0920e111fde9ac6163cab63be651e6053c
sureshatt <suresh.attanayake@gmail.com>
2019-04-13 17:35:51 +0200

    Fixing a typo.

--- spring-boot-oidc-login/src/test/java/com/sureshatt/springsecurity/oidc/OidcLoginApplicationTests.java
+++ spring-boot-oidc-login/src/test/java/com/sureshatt/springsecurity/oidc/OidcLoginApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class OidcLogicApplicationTests {
+public class OidcLoginApplicationTests {
 
 	@Test
 	public void contextLoads() {

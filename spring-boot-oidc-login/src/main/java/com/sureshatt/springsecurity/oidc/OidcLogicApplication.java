256e69d0e8d48629bae152ca6c9963b24283f94f
sureshatt <suresh.attanayake@gmail.com>
2019-04-13 15:46:57 +0200

    Initial code from the spring initializr.

---
+++ spring-boot-oidc-login/src/main/java/com/sureshatt/springsecurity/oidc/OidcLogicApplication.java
@@ -0,0 +1,13 @@
+package com.sureshatt.springsecurity.oidc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OidcLogicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OidcLogicApplication.class, args);
+	}
+
+}
---
+++ spring-boot-oidc-login/src/test/java/com/sureshatt/springsecurity/oidc/OidcLogicApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sureshatt.springsecurity.oidc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OidcLogicApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
